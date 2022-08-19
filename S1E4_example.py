22edf2f81ab3f40f52f83e98afa3e4763b635479
flatearthproove <111557789+flatearthproove@users.noreply.github.com>
2022-08-18 22:57:22 -0700

    Create S1E4_example.py

---
+++ S1E4_example.py
@@ -0,0 +1,6 @@
+from vpython import *    #import vpython library
+from time import *       #import time library
+myobject=cylinder(color=color.blue, Length=1, radius=20)  #making cylinder
+
+while True:  #keep drawing
+	pass
